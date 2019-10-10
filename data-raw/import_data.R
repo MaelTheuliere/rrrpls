1405ec6726d03d74dfc991681d18a6f9a74f53d7
Maël Theulière <mael.theuliere@gmail.com>
2019-10-10 18:57:30 +0200

    - fix data-raw

--- data-raw/import_data.R
+++ data-raw/import_data.R
@@ -1,4 +1,5 @@
-
+library(tidyverse)
+library(TidyRpls)
 indicateurs_rpls_2018<-TidyRpls::indicateurs_rpls_2018 %>% filter(TypeZone != "Communes")
 rpls_par_date_2018 <- TidyRpls::rpls_par_date_2018
 use_data(indicateurs_rpls_2018, overwrite = T, internal = F,compress = "xz")
